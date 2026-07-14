1efbcf58eac083981a7423619ad112fc10511481
Paramesh <paramesh.java5@gmail.com>
2026-07-14 09:48:54 +0530

    Homepage narrative: hero, why-story, belief quote, promise & impact (#37)

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,18 @@
+import Hero from "../components/Hero";
 import JourneyGraphic from "../components/JourneyGraphic";
+import WhyStory from "../components/WhyStory";
 import FoundersMessage from "../components/FoundersMessage";
 
 export default function Home() {
   return (
     <main className="page">
+      <Hero />
+
       <section className="journey">
         <JourneyGraphic />
       </section>
+
+      <WhyStory />
 
       <FoundersMessage />
     </main>
